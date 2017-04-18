d42f5edc518d1c33a1bb9a2acb6e14331e192506
vjramirez <vjramirez@gmail.com>
2017-04-18 15:49:03 +0200

    created the Document Classification tagger

---
+++ src/main/java/eus/ixa/ixa/pipe/ml/polarity/DocumentClassificationTagger.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2017 Victor Ramirez
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package eus.ixa.ixa.pipe.ml.polarity;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+
+public class DocumentClassificationTagger {
+	
+	private final static char tabDelimiter = '\t';
+	private final static char NewLineDelimiter = '\n';
+	private final DoccatModel model;
+	
+	public DocumentClassificationTagger(final String inputModel) throws IOException {
+		
+		InputStream is = new FileInputStream(inputModel);
+		model = new DoccatModel(is);
+	}
+	
+	public String classify(final String document) {
+		DocumentCategorizerME myCategorizer = new DocumentCategorizerME(model);
+		double[] outcomes = myCategorizer.categorize(document);
+		String category = myCategorizer.getBestCategory(outcomes);
+		return document + tabDelimiter + category + NewLineDelimiter;
+	}
+}
